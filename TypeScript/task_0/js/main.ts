88f346d571f2d36e9e607edcdffe8840c861faf0
DESKTOP-G3APRI2\user <houssam.khaled@lau.edu>
2025-07-24 18:46:21 +0300

    lol

---
+++ TypeScript/task_0/js/main.ts
@@ -0,0 +1,48 @@
+
+
+interface Student{
+    firstName: string;
+    lastName: string;
+    age:number;
+    location:string;
+}
+
+const student1: Student = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25,
+    location: "Washington"
+};
+
+const student2: Student = {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 25,
+    location: "Arisona"
+};
+
+
+const studentsList: Student[] = [student1, student2];
+
+function renderTable(students: Student[]){
+    const table = document.createElement("table");
+    const headerRow = table.insertRow();
+    const headerCell1 = headerRow.insertCell(0);
+    const headerCell2 = headerRow.insertCell(1);
+
+    headerCell1.innerText = 'First Name';
+    headerCell2.innerText = 'Location';
+
+    students.forEach(student => {
+        const row = table.insertRow();
+        const cell1 = row.insertCell(0);
+        const cell2 = row.insertCell(1);
+
+        cell1.innerText = student.firstName;
+        cell2.innerText = student.location;
+    });
+
+    document.body.appendChild(table);
+}
+
+renderTable(studentsList);
